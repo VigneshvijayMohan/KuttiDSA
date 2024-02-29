806d35f22c6a916d023bedb04641b9fa4c551c50
Vignesh Mohan <so@yahoo.com>
2024-02-29 09:52:00 +0530

    Changes

--- ds14.py
+++ ds14.py
@@ -1,6 +1,6 @@
 def sqmatrix(no):
     for i in range(1, no + 1):
-        for x in range(65, 65 + i):  # ASCII value for 'A' is 65
+        for x in range(65, 65 + i):
             print(chr(x), end="")
         print("")
 
